da0f3bbd1005c74da2b92322b207577e51a18324
manpintault <pintault.manon@live.fr>
2014-03-05 03:12:09 -0800

    mofification fonction distance

--- point.c
+++ point.c
@@ -44,7 +44,7 @@ float distanceEntreDeuxPoints(point p1, point p2)
   double x2 = getX(p2);
   double y1 = getY(p1);
   double y2 = getY(p2);
-  double tmp =  (x2 - x1) + (y2 - y1);
+  float tmp =  (x2 - x1) + (y2 - y1);
 	if (tmp<0)
 		tmp=-tmp;
   return tmp;
