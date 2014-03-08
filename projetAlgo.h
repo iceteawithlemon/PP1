5db722610efc37770106b07589c2c3d6aaa84640
bensaintsever <benjamin.saint-sever@etu.u-bordeaux.fr>
2014-03-08 12:18:33 -0800

    Interface mis à jour

---
+++ projetAlgo.h
@@ -0,0 +1,16 @@
+//
+//  projetAlgo.h
+//  ProjetPP1
+//
+//  Created by Benjamin Saint-Sever on 28/02/2014.
+//  Copyright (c) 2014 Benjamin Saint-Sever. All rights reserved.
+//
+
+#ifndef ProjetPP1_projetAlgo_h
+#define ProjetPP1_projetAlgo_h
+
+#include "Liste.h"
+
+point PointLePlusProche(point p,list l);
+
+#endif
