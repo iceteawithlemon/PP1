f5e74b0f37089606c71e5dc7f1baf2cb98462928
bensaintsever <benjamin.saint-sever@etu.u-bordeaux.fr>
2014-03-08 12:18:51 -0800

    fonction mis à jour

--- projetAlgo.c
+++ projetAlgo.c
@@ -1,31 +1,18 @@
+#include "point.h"
+#include "Liste.h"
 
 
-Define NombreDePoints 10
-
-
-
-point TabPoints[NombreDePoints];
-
-point p0 = CreerPoints(0,0);
-TabPoints[0] = p0;
-point p1 = CreerPoints(4,5);
-TabPoints[1] = p1;
-point p2 = CreerPoints(2,4);
-TabPoints[2] = p2;
-
-
-//Indice --> point actuel
-int ProchainPoint(point p0, int indice){
-  double DProchainPoint = LongeurEntre2Points(p0,TabPoints[++indice]);
-  int indiceDuPlusCourt = indice;
-
-  for(; indice<NombreDePoints-2;indice++){
-    double plusProche = LongeurEntre2Points(p0,TabPoints[indice+1]);
+point PointLePlusProche(point p,list liste){
+    point pointTmp = creerPoint(0,0);
+    double distance = 0;
+    double tmp = 0;
+    distance = distanceEntreDeuxPoints(p,valeur(liste));
     
-    if (plusProche < DProchainPoint){
-      DProchainPoint = PlusProche;
-      indiceDuPlusCourt = indice;
+    while (!finListe(liste)) {
+        tmp = distanceEntreDeuxPoints(p,valeur(liste));
+        if (tmp<distance) {
+            pointTmp = valeur(liste);
+        }
     }
-  }
-  return indiceDuPlusCourt;
+    return pointTmp;
 }
