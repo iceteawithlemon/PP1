86d19c3c750be22addabcfaface07a7ef5d47c6d
bensaintsever <benjamin.saint-sever@etu.u-bordeaux.fr>
2014-03-10 05:56:59 -0700

    Modification de la fonction PointLePlusProche afin de la rendre fonctionnelle,ainsi que l'ajout de commentaires

--- projetAlgo.c
+++ projetAlgo.c
@@ -1,17 +1,41 @@
 #include "point.h"
 #include "Liste.h"
+# include <stdlib.h>
+
 
 
 point PointLePlusProche(point p,list liste){
     point pointTmp = creerPoint(0,0);
     double distance = 0;
     double tmp = 0;
+    
+    //Distance entre p et le prochain point de la liste
     distance = distanceEntreDeuxPoints(p,valeur(liste));
     
-    while (!finListe(liste)) {
-        tmp = distanceEntreDeuxPoints(p,valeur(liste));
+    //Stockage du point suceptible d'etre le plus proche
+    pointTmp = clone(valeur(liste));
+    
+    //Copie de la liste afin d'avancer
+    list listeTmp = liste;
+    
+    
+    //Tant que l'on est pas à la fin de la liste
+    while (!finListe(listeTmp)) {
+        
+        //On passe au prochain point de la liste
+        listeTmp = suivant(listeTmp);
+        
+        //Distance entre p et le point d'après
+        tmp = distanceEntreDeuxPoints(p,valeur(listeTmp));
+        
+        //Si le point d'après est plus près
         if (tmp<distance) {
-            pointTmp = valeur(liste);
+            
+            //On garde le point le plus proche
+            pointTmp = clone(valeur(listeTmp));
+            
+            //On garde la distance entre p et le point le plus proche comme référence distance
+            distance = tmp;
         }
     }
     return pointTmp;
