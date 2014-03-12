c4788cc40368aafa45de0f857b129220d4709e46
manpintault <pintault.manon@live.fr>
2014-03-12 12:14:12 +0100

    modification modulaire du dossier générale

--- projetAlgo.h
+++
@@ -1,9 +0,0 @@
-#ifndef ProjetPP1_projetAlgo_h
-#define ProjetPP1_projetAlgo_h
-
-#include "Liste.h"
-
-point PointLePlusProche(point p,list l);
-point* nearestNeighbour(int nombreDePoints,point* tab,point* ordreDePassage);
-
-#endif
