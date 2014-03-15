f26da2572d5db3fa86f4c052c89ab586e53a18f2
iceteawithlemon <caitlin@lesfragnes.com>
2014-03-15 22:13:31 +0100

    ajout differenciation float/int pour ecriture de fichier tsp

--- Matrice.c
+++ Matrice.c
@@ -62,7 +62,7 @@ void detruireMatrice(matrice m)
 
 //primitives d'accès
 
-int getIndicePoint(matrice m, point p)
+int getIndicePoint(matrice m, point p) //retourne l'indice d'un point, ou -1 s'il n'existe pas
 {
 	for(int i = 0; i < m->dimension; i++)
 		if (equals(p, m->ref[i]))
@@ -70,17 +70,17 @@ int getIndicePoint(matrice m, point p)
 	return -1;	
 }
 
-point getPointIndice(matrice m, int indice)
+point getPointIndice(matrice m, int indice) //retourn un point à partir de son indice
 {
 	return m->ref[indice];
 }
 
-double getDistanceIndice(matrice m, int ref1, int ref2)
+float getDistanceIndice(matrice m, int ref1, int ref2) //retourne la distance entre deux points referencés par les deux indices données
 {
 	return m->tab[ref1][ref2];
 }
 
-double getDistancePoint(matrice m, point p1, point p2)
+float getDistancePoint(matrice m, point p1, point p2) // retourne la distance entre deux points
 {
 	return getDistanceIndice(m, getIndicePoint(m, p1), getIndicePoint(m, p2));
 }
@@ -88,8 +88,9 @@ double getDistancePoint(matrice m, point p1, point p2)
 
 // affichage
 
-void afficherMatrice(matrice m)
+void afficherMatrice(matrice m) //affiche la matrice passée en paramètre
 {
+	assert(m != NULL);
 	printf("Dimension: %d\n", m->dimension);
 	printf("Points: \n");
 	for(int i = 0; i < m->dimension; i++)
@@ -125,7 +126,7 @@ void fermer_tsp(FILE *fp)
 	fclose(fp);
 }
 
-matrice lecture_tsp(FILE *fp)
+matrice lecture_tsp(FILE *fp) //crée un matrice à partir d'un fichier TSP de format 'FULL_MATRIX'
 {
 	char str[1000];
 	char *tok;
@@ -134,8 +135,8 @@ matrice lecture_tsp(FILE *fp)
 	int i = 0;
 	int j = 0;
 	int pi = 0;
-	int px = 0;
-	int py = 0;
+	float px = 0;
+	float py = 0;
 	int k = 0;
 	int dim_bool = 0;
 	int dim;
@@ -149,37 +150,29 @@ matrice lecture_tsp(FILE *fp)
 		tok = strtok(str, ":");
 		while(tok != NULL && x == 0)
 		{
-			printf("tok: '%s'\n", tok);
+			//printf("tok: '%s'\n", tok);
 			if(dim_bool)
 			{
 				dim = atoi(tok);
-				printf("dim: %d\n", dim);
+				//printf("dim: %d\n", dim);
 				dim_bool = 0;
 				m = creerMatriceVide(dim);
 			}
 			if(!strcmp(tok, "DIMENSION"))
 				dim_bool = 1;
-			/*if(!strcmp(tok, "EDGE_WEIGHT_FORMAT") && strcmp(strtok(NULL, ": "), "FULL_MATRIX"))
-				exit(0);*/
 			if (!strcmp(tok, "EDGE_WEIGHT_SECTION"))
 				x=1;
-			//printf("(%s)\n", tok);
 			tok = strtok(NULL, delim);
 		}
-		//j = 0;
 		if(x == 1)
 		{
 			if(i >= dim)
 				y = 1;
 			delim = " ";
-			//strcpy(tmp, str);
 			tok = strtok(str, delim);
-			//strtok(NULL, delim);
-			//printf("tok: %s, y=%d\n", tok, y);
-			//i++;
 			while(tok != NULL && y == 0 && !skip && i < dim)
 			{
-				printf("i, j: %d, %d = %.2f\n", i, j, atof(tok));
+				//printf("i, j: %d, %d = %.2f\n", i, j, atof(tok));
 				m->tab[i][j] = atof(tok);
 				j++;
 				if(j >= dim)
@@ -195,9 +188,9 @@ matrice lecture_tsp(FILE *fp)
 		}
 		if(y == 1 && k < dim)
 		{
-			fscanf(fp, "%d %d %d\n", &pi, &px, &py);
+			fscanf(fp, "%d %f %f\n", &pi, &px, &py);
 			m->ref[pi-1] = creerPoint(px, py);
-			printf("%d %d %d\n", pi, px, py);
+			//printf("%d %f %f\n", pi, px, py);
 			k++;
 		}
 		if(k >= dim)
@@ -207,21 +200,38 @@ matrice lecture_tsp(FILE *fp)
 
 } 
 
-void ecrireMatriceTSP(char *fnom, matrice m)
+matrice creerMatriceTSP(char *fnom) //wrapper for lecture_tsp
 {
+	FILE *fp = ouvrir_tsp(fnom, "r");
+	matrice m = lecture_tsp(fp);
+	fclose(fp);
+	return m;
+}
+
+
+void creerTSPMatrice(char *fnom, matrice m)
+{
+	
 	FILE *fp = ouvrir_tsp(fnom, "w");
 	assert(fp != NULL);
+	int is_int = ( (int)m->tab[0][1] == m->tab[0][1] ) ;
 	fprintf(fp, "NAME: %s\nTYPE: TSP\nDIMENSION: %d\n", fnom, m->dimension);
 	fprintf(fp, "EDGE_WEIGHT_TYPE: EXPLICIT\nEDGE_WEIGHT_FORMAT: FULL_MATRIX\nDISPLAY_DATA_TYPE: TWOD_DISPLAY\nEDGE_WEIGHT_SECTION\n");
 	for(int i = 0; i < m->dimension; i++)
 	{
 		for(int j = 0; j < m->dimension; j++)
-			fprintf(fp, "%f ", getDistanceIndice(m, i, j));
+			if(is_int)
+				fprintf(fp, "%d ", (int)getDistanceIndice(m, i, j));
+			else
+				fprintf(fp, "%f ", getDistanceIndice(m, i, j));
 		fprintf(fp, "\n");
 	}
 	fprintf(fp, "DISPLAY_DATA_SECTION\n");
 	for(int i = 0; i < m->dimension; i++)
-		fprintf(fp, "\t%d\t%f\t%f\n", i+1, (double)getX(getPointIndice(m, i)), (double)getY(getPointIndice(m, i)));
+		if(is_int)
+			fprintf(fp, "\t%d\t%d\t%d\n", i+1, (int)getX(getPointIndice(m, i)), (int)getY(getPointIndice(m, i)));
+		else
+			fprintf(fp, "\t%d\t%f\t%f\n", i+1, (float)getX(getPointIndice(m, i)), (float)getY(getPointIndice(m, i)));
 	fprintf(fp, "EOF\n");
 	fermer_tsp(fp);
 }
@@ -247,13 +257,10 @@ int main()
 
 	printf("Lecture tsp: \n");
 	char fnom[] = "exemple10.tsp";
-	FILE *fp = ouvrir_tsp(fnom, "r");
-	matrice m = lecture_tsp(fp);
-	fermer_tsp(fp);
+	matrice m = creerMatriceTSP(fnom);
+	
 	afficherMatrice(m);
-
-	char tsp_test[] = "exemple102";
-	ecrireMatriceTSP(tsp_test, m);
+	creerTSPMatrice("test.tsp", m);
 
 	detruireMatrice(test);
 
