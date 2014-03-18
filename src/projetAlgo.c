cd58596927b96d0a6166508566f97a335cd0e804
bensaintsever <benjamin.saint-sever@etu.u-bordeaux.fr>
2014-03-18 12:27:15 +0100

    Modification complete de l'algorithme,prise en compte de matrice + optimisation du code afin de ne plus utiliser les listes chainées

--- src/projetAlgo.c
+++ src/projetAlgo.c
@@ -1,92 +1,88 @@
 #include "point.h"
 #include "Liste.h"
-# include <stdlib.h>
+#include <stdlib.h>
+#include "matrice.h"
 
-
-
-point PointLePlusProche(point p,list liste){
-    point pointTmp = creerPoint(0,0);
+//Version tableau matrice qui renvoi l'indice du point le plus proche
+int PointLePlusProche(int indicePointActuel,matrice m){
+    
     float distance = 0;
     float tmp = 0;
+    int TailleTab = getDimensionMatrice(m);
     
-    //Distance entre p et le prochain point de la liste
-    distance = distanceEntreDeuxPoints(p,valeur(liste));
+    int indice = 0;
     
-    //Stockage du point suceptible d'etre le plus proche
-    pointTmp = clone(valeur(liste));
-    
-    //Copie de la liste afin d'avancer
-    list listeTmp = liste;
+    while(getPointisVisited(m, indice)){
+        indice++;
+    }
     
     
-    //Tant que l'on est pas à la fin de la liste
-    while (!finListe(listeTmp)) {
+    //Distance entre le point actuel et le prochain point de la liste
+    distance = getDistanceIndice(m,indicePointActuel, indice);
+    
+    //Sauvegarde de l'indice du point
+    int indicePoint = indice;
+    
+    //Tant que l'on est pas à la fin du tableau
+    while (indice != TailleTab-1) {
         
-        //On passe au prochain point de la liste
-        listeTmp = suivant(listeTmp);
+        //On passe au prochain point
+        indice++;
         
-        //Distance entre p et le point d'après
-        tmp = distanceEntreDeuxPoints(p,valeur(listeTmp));
-        
-        //Si le point d'après est plus près
-        if (tmp<distance) {
+        if(!getPointisVisited(m, indice)){
             
-            //On garde le point le plus proche
-            pointTmp = clone(valeur(listeTmp));
+            //Distance entre le point actuel et le point d'après
+            tmp = getDistanceIndice(m,indicePointActuel, indice);
             
-            //On garde la distance entre p et le point le plus proche comme référence distance
-            distance = tmp;
+            //Si le point d'après est plus près
+            if (tmp<distance) {
+                
+                //On sauvegarde l'indice du point le plus proche
+                indicePoint = indice;
+                
+                //On garde la distance entre p et le point le plus proche comme référence distance
+                distance = tmp;
+            }
         }
     }
-    return pointTmp;
+    setMatricePointMarkVisited(m, indicePoint);
+    return indicePoint;
 }
 
-
-
-
-point* nearestNeighbour(int nombreDePoints,point tab[],point ordreDePassage[]){
-    //Initialisation d'une liste chainée
-    list liste = NULL;
+matrice nearestNeighbour(matrice m,point tab[],point ordreDePassage[]){
     
-    //Insertion du premier point dans la liste (après le point d'origine)
-    liste = ajouterEnTete(liste, tab[1]);
+    int nombreDePoints = getDimensionMatrice(m);
+    point tabPoint[nombreDePoints];
     
-    //Insertion des points dans la liste
-    for(int i = 2; i<nombreDePoints;i++){
-        liste = ajouterEnFin(liste, tab[i]);
-    }
+    //Creation d'une matrice de sortie
+    matrice mOut = creerMatriceDesPoints(tabPoint,nombreDePoints+1);
     
-    //Point d'origine inséré dans le tableau
-    ordreDePassage[0] = clone(tab[0]);
+    
+    //Point d'origine inséré dans la nouvelle matrice
+    setMatricePoint(mOut,clone(getPointIndice(m,0)),0);
+    setMatricePointMarkVisited(m,0);
     
     //On defini un point qui sera l'intermédiaire
-    point pointActuel = clone(tab[0]);
+    int indicePointActuel = 0;
     
     //Indice du tableau d'ordre de passage
     int indice = 1;
     
-    //Tant que l'on est pas à la fin de la liste
-    while(indice<nombreDePoints){
+    //Tant que l'on est pas à la fin de la matrice
+    while(indice<=nombreDePoints-1){
         
-        // On stocke le point le plus proche dans le point intermédiaire
-        pointActuel = clone(PointLePlusProche(pointActuel, liste));
+        indicePointActuel = PointLePlusProche(indicePointActuel,m);
         
-        // On supprime le point trouvé de la liste
-        liste = supprimerElement(liste, pointActuel);
-        
-        
-        // On implémente le point le plus proche du précedent dans le tableau
-        ordreDePassage[indice] = clone(pointActuel);
+        // On implémente le point le plus proche du précedent dans la matrice de sortie
+        setMatricePoint(mOut,getPointIndice(m,indicePointActuel),indice);
         
         // On passe à l'indice suivant
         indice++;
     }
+    
     // On ajoute le point de départ au tableau d'ordre de passage
-    ordreDePassage[indice] = clone(tab[0]);
+    setMatricePoint(mOut,clone(getPointIndice(m,0)),indice);
     
-    // On libere la mémoire de la liste
-    effacerListe(liste);
-    
-    return ordreDePassage;
+    return mOut;
 }
 
