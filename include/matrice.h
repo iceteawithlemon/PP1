bf686b7cdc60ad18d807902363f380be7ae1d2c4
bensaintsever <benjamin.saint-sever@etu.u-bordeaux.fr>
2014-03-22 18:46:00 +0100

    Suppresion des fonctions de marquage des points

--- include/matrice.h
+++ include/matrice.h
@@ -2,6 +2,7 @@
 #define MATRICE_H
 
 # include "point.h"
+# include <stdio.h>
 
 typedef struct Matrice *matrice;
 
@@ -16,8 +17,6 @@ float getDistancePoint(matrice m, point p1, point p2);
 
 int getDimensionMatrice(matrice m);
 void setMatricePoint(matrice mOut,point p,int indice);
-void setMatricePointMarkVisited(matrice m, int indice);
-bool getPointisVisited(matrice m, int indice);
 
 void afficherMatrice(matrice m);
 
@@ -26,5 +25,8 @@ matrice creerMatriceTSP(char *fnom);
 void creerTSPMatrice(char *fnom, matrice m);
 void creerTOUR(char *fnom, matrice m, point liste[]);
 
+
+matrice lecture_tsp(FILE *fp);
+FILE *ouvrir_tsp(char *fnom, char *mode);
 
 #endif
