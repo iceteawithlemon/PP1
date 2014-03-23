2c8184d873218f49a4bf0c71ced3a6640ee14a09
bensaintsever <benjamin.saint-sever@etu.u-bordeaux.fr>
2014-03-23 12:23:15 +0100

    Main permettant de tester le programme, je n'ai pas reussi a lire le fichier bays29.tsp avec lectureTSP()

--- src/main.c
+++ src/main.c
@@ -2,41 +2,46 @@
 #include <stdlib.h>
 #include "point.h"
 #include "projetAlgo.h"
-#include "Liste.h"
+#include "matrice.h"
 
 #define nombreDePoints 4
 int main(int argc, const char * argv[])
 {
-
+    
     //Creation de Points (a remplacer par une lecture de matrice)
-    point p1 = creerPoint(0,0);
-	point p2 = creerPoint(1,6);
-	point p3 = creerPoint(2,0);
-	point p4 = creerPoint(3,0);
+     point p1 = creerPoint(0,0);
+     point p2 = creerPoint(1,6);
+     point p3 = creerPoint(2,0);
+     point p4 = creerPoint(3,0);
+     
+     
+     
+     //Tableau comprenant les points
+     point tab[nombreDePoints]={p1,p2,p3,p4};
+     
+     printf("Points en entrée : \n");
+     for(int i =0;i<nombreDePoints;i++){
+     afficherPoint(tab[i]);
+     }
+     
+
     
+    /*char* mode = "r";
+    //char* fileName = "bays29.tsp";
+    char fileN [11] = "bays29.tsp";
+    //ouverture du fichier
+    FILE* tsp = ouvrir_tsp(fileN, mode);
     
+    matrice matriceIN;
+    //Lecture du fichier
+    matriceIN = lecture_tsp(tsp);
     
-    //Tableau comprenant les points
-	point tab[nombreDePoints]={p1,p2,p3,p4};
+    afficherMatrice(matriceIN);*/
     
-    printf("Points en entrée : \n");
-    for(int i =0;i<nombreDePoints;i++){
-        afficherPoint(tab[i]);
-    }
-   
-    //Creation d'un tableau qui contiendra l'ordre de passage le plus court
-    point ordreDePassage[nombreDePoints+1] ;
-    
-    //Appel à la fonction nearestNeighbour qui calcule le chemin le plus court
-    nearestNeighbour(nombreDePoints,tab,ordreDePassage);
-    
-    
-    printf("Points en sortie :  \n");
-    //Affichage de l'ordre de passage
-    for(int i =0;i<nombreDePoints+1;i++){
-        afficherPoint(ordreDePassage[i]);
-    }
-    
+    //int nbPoint = getDimensionMatrice(matriceIN);
+    //point ordreDePassage[nbPoint+1];
+    //nearestNeighbour(matriceIN,ordreDePassage);
+
     return 0;
 }
 
