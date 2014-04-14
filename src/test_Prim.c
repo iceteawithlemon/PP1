5ce1b288540fe67f9d1b94ed4662c4329aeef448
manpintault <pintault.manon@live.fr>
2014-04-14 16:55:49 +0200

    ajout test_Prim.c

---
+++ src/test_Prim.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "point.h"
+#include "projetAlgo.h"
+#include "matrice.h"
+# include "tspIOtourO.h"
+
+#define nombreDePoints 4
+int main()
+{
+    
+    //Creation de Points (a remplacer par une lecture de matrice)
+     point p1 = creerPoint(0,0);
+     point p2 = creerPoint(1,1);
+     point p3 = creerPoint(2,2);
+     point p4 = creerPoint(3,3);
+     
+    
+     //Tableau comprenant les points
+     point tab[nombreDePoints]={p4 ,p2, p1, p3};
+     
+     printf("Points en entrée : \n");
+     for(int i =0;i<nombreDePoints;i++)
+        afficherPoint(tab[i]);
+     
+    
+    //CREATION MATRICE ENTREE
+    matrice matriceIN;
+    matriceIN = creerMatriceDesPoints(tab, nombreDePoints);
+    
+    //AFFICHAGE MATRICE
+    matrice m = creerMatriceTSP("../test_cases/exemple10.tsp");
+
+    afficherMatrice(m);
+     
+    //RECUPERATION DU NOMBRE DE POINTS
+    int nbPointIn = getDimensionMatrice(matriceIN);
+    int nbPointOut = nbPointIn+1;
+    
+    //CREATION D'UN TABLEAU DE POINT
+    point ordreDePassage[nbPointOut];
+    
+   
+   //PRIM -> Segmentation Fault :(
+    printf("Test prim:\n");
+    point tabPrim[nbPointOut];
+    int dist = prim(m,tabPrim);
+    matrice matricePrimOut = creerMatriceDesPoints(tabPrim, nbPointOut);
+    afficherListeDesPoints(tabPrim, nbPointOut);
+    
+    printf("\nDistance totale REELLE !!! prim : %d\n",dist);
+    printf("\nDistance totale: %d\n", overallDistanceVerbose(matricePrimOut, tabPrim));
+     
+
+    return 0;
+}
+
